052eedb7aeff242de388a79491b936f7a8c4b9a7
afiqster12 <2200546@sit.singaporetech.edu.sg>
2024-01-18 13:55:45 +0800

    Changed name to Elon Musk

--- app/src/main/java/com/example/helloinf2007/MainActivity.kt
+++ app/src/main/java/com/example/helloinf2007/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column {
                         Greeting("INF2007")
-                        From(name = "Afiq and friends")
+                        From(name = "Elon Musk")
                     }
                 }
             }
